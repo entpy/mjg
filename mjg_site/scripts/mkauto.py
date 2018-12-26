7ea76d896e6283bb4f2c2c5c3f9446d5db7c7e11
entpy <developer@entpy.com>
2018-12-26 18:53:22 +0100

    working on mkauto images

--- mjg_site/scripts/mkauto.py
+++ mjg_site/scripts/mkauto.py
@@ -141,8 +141,6 @@ def run():
             ma_event_obj.make_event(user_id=single_account["id"], ma_code=current_ma_event, strings_ma_code=strings_ma_code, ma_code_dictionary=single_event_dictionary)
     ##### Chiedo all'utente di lasciare una recensione (informazioni pubbliche) }}} #####
 
-    # OK
-    """
     ##### Premio con un testo random  {{{ #####
     current_ma_event = "random_promo"
     account_list = None
@@ -160,10 +158,7 @@ def run():
         for single_account in account_list:
             ma_event_obj.make_event(user_id=single_account["id"], ma_code=current_ma_event, strings_ma_code=strings_ma_code, ma_code_dictionary=single_event_dictionary)
     ##### Premio con un testo random  }}} #####
-    """
 
-    # OK
-    """
     ##### Tip o info random {{{ #####
     current_ma_event = "random_tip"
     account_list = None
@@ -182,4 +177,3 @@ def run():
         for single_account in account_list:
             ma_event_obj.make_event(user_id=single_account["id"], ma_code=current_ma_event, strings_ma_code=strings_ma_code, ma_code_dictionary=single_event_dictionary)
     ##### Tip o info random }}} #####
-    """
